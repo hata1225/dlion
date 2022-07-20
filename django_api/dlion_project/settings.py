b0d85bda02187ec656d7247fe6481b5a538e8ed4
hatanaka <y.hata1225@gmail.com>
2022-07-20 21:48:46 +0900

    settings.py BASE_DIR変更

--- django_api/dlion_project/settings.py
+++ django_api/dlion_project/settings.py
@@ -15,7 +15,7 @@ from django.core.management.utils import get_random_secret_key
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'dlion_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
