5c8b1a8c8d43e985c389dc995b26caa4848ce2d6
hatanaka <y.hata1225@gmail.com>
2023-04-12 15:56:14 +0900

    フロント側のgoogle auth

---
+++ src/components/GoogleAuthButton/index.tsx
@@ -0,0 +1,46 @@
+import { Button, makeStyles } from "@material-ui/core";
+import { useGoogleLogin } from "@react-oauth/google";
+import { googleOauth } from "api/api";
+import { ReactComponent as GoogleIcon } from "./google.svg";
+
+interface Props {
+  text?: string;
+}
+
+export const GoogleAuthButton = ({ text = "googleでログイン" }: Props) => {
+  const classes = useStyles();
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse: any) => {
+      const authresponse = await googleOauth(tokenResponse.access_token);
+      console.log("authres: ", authresponse);
+    },
+  });
+
+  return (
+    <Button
+      className={classes.googleAuthButton}
+      variant="outlined"
+      onClick={async () => await login()}
+      fullWidth
+    >
+      <div className={classes.googleAuthButtonInner}>
+        <GoogleIcon width={20} height={20} />
+        <p>{text}</p>
+      </div>
+    </Button>
+  );
+};
+
+const useStyles = makeStyles({
+  googleAuthButton: {
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+  },
+  googleAuthButtonInner: {
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    textTransform: "none",
+  },
+});
